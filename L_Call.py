321c54f3ebfa463923b3b28772fdd436b1560e43
Isai <isai_alejandro@outlook.com>
2023-08-07 10:33:53 -0600

    plot del call largo

--- L_Call.py
+++ L_Call.py
@@ -9,7 +9,7 @@ with open("Settings/fee_settings.json", "r") as file:
 with open("Settings/call_settings.json", "r") as file:
     call_data = json.load(file)
 
-#Funciones y declaraciones
+#%% Funciones y declaraciones
 data = pd.DataFrame(columns = ["Precio", "G. Bruta", "G. Neta"])
 # Weighted premium
 premiumNeto : float = call_data["Premium"]*call_data["Participacion"]
@@ -30,11 +30,23 @@ def closeFee(cFee : float, numero : int, close : float, strike : float, contrato
 def optionVal(gBruta : float, contratos : int, participacion : float, multiplicador : float, numero : int) -> float:
     return np.where(gBruta <= 0, gBruta*contratos, (gBruta*participacion*multiplicador*numero)*contratos)
 
-#Data
+#%% Data
 data["Precio"] = np.arange(call_data["Lim. Inferior"], call_data["Lim. Superior"]+1, call_data["Granularidad"])
 data["G. Bruta"] = np.round(call_payoff(data["Precio"], call_data["Strike"], premiumNeto, openFee), 3)
 data["G. Neta"] = np.round(optionVal(data["G. Bruta"], call_data["Contratos"],
                             call_data["Participacion"], call_data["Multiplicador"],
                             call_data["#"]) - closeFee(fee_data["Close fee"], call_data["#"],
                             data["Precio"], call_data["Strike"], call_data["Contratos"]), 3)
-data.to_csv("prueba.csv", index = False)
+#%% Plot
+plt.figure(figsize=(7,5))
+plt.title("Utilidad bruta")
+plt.gca().spines["top"].set_visible(False)
+plt.gca().spines["right"].set_visible(False)
+plt.axhline(y=0, linestyle="dashed", c="black")
+plt.plot(data["Precio"], data["G. Bruta"])
+plt.show()
+
+#%% Comentarios
+"""
+1) Hay que automatizar el ajuste de granularidad
+"""
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ### Referencias
+* https://docs.google.com/spreadsheets/d/1aC04DyfDFYNIq7DyonNic8fJ0-y9CVJn3kdCaCkNCEw/edit?usp=sharing
 * https://www.macroption.com/calculating-option-payoff-in-excel/
 * https://www.binance.com/en/support/faq/binance-options-trading-fees-5326e5de61c34fed98abe28d2f175a23
 * https://stackoverflow.com/questions/36921951/truth-value-of-a-series-is-ambiguous-use-a-empty-a-bool-a-item-a-any-o
